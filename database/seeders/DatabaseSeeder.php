0c59148d1c4d9d2394ebe1eb580dd3fcc966fb16
William <william.hausmann@cpnv.ch>
2020-12-01 11:11:33 +0100

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(OpinionsTableSeeder::class);
+        $this->call(OpinionstatesTableSeeder::class);
+        $this->call(ReferencesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(ThemesTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(OpinionstatetransitionsTableSeeder::class);
+        $this->call(OpinionsHasReferencesTableSeeder::class);
+        $this->call(UsersCommentOpinionsTableSeeder::class);
     }
 }
