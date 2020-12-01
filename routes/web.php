7f8c1dc11f6ed933ee7c264ac8d4dd9a5429a198
William <william.hausmann@cpnv.ch>
2020-12-01 11:07:31 +0100

    Add ressources

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OpinionController;
+use App\Http\Controllers\ThemeController;
+use App\Http\Controllers\ReferenceController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\StateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+Route::resource('opinions',OpinionController::class);
+Route::resource('references',ReferenceController::class);
+Route::resource('roles',RoleController::class);
+Route::resource('states',StateController::class);
+Route::resource('themes',ThemeController::class);
