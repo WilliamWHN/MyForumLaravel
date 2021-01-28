15dc7420e81641100b6db2cde48b61dbc4b2620b
Hausmann <william.hausmann@cpnv.ch>
2021-01-28 12:02:29 +0100

    Add role name to the list

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,14 +4,14 @@
     <h1 class="text-center p-5">Listes des utilisateurs</h1>
     @forelse ($users as $user)
         @if ($admins < 5)
-        <div class="border border-light p-2 ml-2 text-decoration-none"><div class="text-uppercase">{{$user->pseudo}}</div>
+        <div class="border border-light p-2 ml-2 text-decoration-none"><div class="text-uppercase">{{$user->pseudo}} - {{$user->role->name}}</div>
             @if($user->role->slug !== 'ADMI')
                 <a href="{{ route('users.edit',$user->id) }}"><div><button type="button" class="btn btn-sm btn-success">Nommer Admin</button></div>
             @elseif($user->id !== Auth::user()->id)
                 <a href="{{ route('users.edit',$user->id) }}"><div><button type="button" class="btn btn-sm btn-danger">Destituer</button></div>
             @endif</div></a>
         @else
-            <div class="border border-light p-2 ml-2 text-decoration-none"><div>{{$user->pseudo}}</div>
+            <div class="border border-light p-2 ml-2 text-decoration-none"><div>{{$user->pseudo}} - {{$user->role->name}}</div>
             @if($user->role->slug === 'ADMI')
                     <a href="{{ route('users.edit',$user->id) }}"><div><button type="button" class="btn btn-sm btn-danger">Destituer</button></div>
             @endif
