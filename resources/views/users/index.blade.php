07395fa47e1153d6004eef87fed18a6fd34acdf1
Hausmann <william.hausmann@cpnv.ch>
2021-01-28 11:58:12 +0100

    Restyle user list index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,16 +4,16 @@
     <h1 class="text-center p-5">Listes des utilisateurs</h1>
     @forelse ($users as $user)
         @if ($admins < 5)
-        <div class="border border-light p-2 ml-2 text-decoration-none">{{$user->pseudo}}
+        <div class="border border-light p-2 ml-2 text-decoration-none"><div class="text-uppercase">{{$user->pseudo}}</div>
             @if($user->role->slug !== 'ADMI')
-                <a href="{{ route('users.edit',$user->id) }}">Nommer Admin
+                <a href="{{ route('users.edit',$user->id) }}"><div><button type="button" class="btn btn-sm btn-success">Nommer Admin</button></div>
             @elseif($user->id !== Auth::user()->id)
-                <a href="{{ route('users.edit',$user->id) }}">Destituer
+                <a href="{{ route('users.edit',$user->id) }}"><div><button type="button" class="btn btn-sm btn-danger">Destituer</button></div>
             @endif</div></a>
         @else
-            <div class="border border-light p-2 ml-2 text-decoration-none">{{$user->pseudo}}
+            <div class="border border-light p-2 ml-2 text-decoration-none"><div>{{$user->pseudo}}</div>
             @if($user->role->slug === 'ADMI')
-                    <a href="{{ route('users.edit',$user->id) }}">Destituer
+                    <a href="{{ route('users.edit',$user->id) }}"><div><button type="button" class="btn btn-sm btn-danger">Destituer</button></div>
             @endif
             </div>
         @endif
