fafbee2afecb730a15c98e4e4e8aefa51a279816
William <william.hausmann@cpnv.ch>
2021-02-09 12:06:54 +0100

    Modify capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.15.0'
 set :application, 'My_Forum'
 set :repo_url, 'git@github.com:WilliamWHN/MyForumLaravel.git'
 # Default branch is :master
-set :branch, ENV["branch"] || "Dev"
+set :branch, ENV["branch"] || "master"
 # Default deploy_to directory is /var/www/laravel-capistrano
 set :deploy_to, '/home/cld2_12/cld2-12.mycpnv.ch'
 set :use_sudo, false
@@ -18,6 +18,7 @@ append :linked_dirs,
     'storage/framework/sessions',
     'storage/framework/views',
     'storage/logs'
+
 namespace :composer do
     desc "Running Composer Install"
     task :install do
@@ -33,23 +34,19 @@ task :set_php_version do
     execute "ls /home/cld2_12/.data/cld2-12.mycpnv.ch_php* 2>/dev/null | sed -E 's/.+(php[[:digit:]]+)$/\\1/' >/tmp/.php-cli-version"
   end
 end
-namespace :laravel do
-    task :fix_permission do
-        on roles(:laravel) do
-            execute :chmod, "-R ug+rwx #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
-            execute :chgrp, "-R www-data #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
-        end
-    end
-    task :configure_dot_env do
-    dotenv_file = fetch(:laravel_dotenv_file)
-        on roles (:laravel) do
-        execute :cp, "#{dotenv_file} #{release_path}/.env"
-        end
-    end
+# Copy .env in the current release
+task :copy_dotenv do
+   on roles(:all) do
+     execute :cp, "#{shared_path}/.env #{release_path}/.env"
+   end
 end
 before 'composer:run', 'set_php_version'
 namespace :deploy do
     after :updated, "composer:install"
-    after :updated, "laravel:fix_permission"
-    after :updated, "laravel:configure_dot_env"
+    after :updated, 'copy_dotenv'
 end
+after  'copy_dotenv', 'laravel:migrate'
+
+Rake::Task['laravel:optimize'].clear_actions rescue nil
+Rake::Task['deploy:set_permissions:acl'].clear_actions rescue nil
+
